d37fbff8ea6f0d83960a592435f761ab347bec7f
Olga Pogrebňa <olga.pogrebnya@live.com>
2024-04-11 14:54:21 +0200

    Change hra.js according to the task #3

--- hra.js
+++ hra.js
@@ -1,32 +1,31 @@
+
+
 let currentPlayer = 'circle';
-
-const selectCircle = (event) => {
+const CircleorCross = (event) => {
+  event.target.disabled = true;
+if (currentPlayer === 'circle') {
   event.target.classList.add('board__field--circle');
-};
-
-const selectCross = (event) => {
+  const imgCross = document.querySelector('.image-player');
+  imgCross.src = 'image/cross.svg';
+  currentPlayer = 'cross';
+}
+else {
   event.target.classList.add('board__field--cross');
+  const imgCircle = document.querySelector('.image-player');
+  imgCircle.src = 'image/circle.svg';
+  currentPlayer = 'circle';
+}
 };
 
-
-document.querySelector('button:nth-child(1)').addEventListener('click', selectCircle);
-
-document.querySelector('button:nth-child(2)').addEventListener('click', selectCross);
-
-document.querySelector('button:nth-child(3)').addEventListener('click', selectCircle);
-
-document.querySelector('button:nth-child(4)').addEventListener('click', selectCross);
-
-document.querySelector('button:nth-child(5)').addEventListener('click', selectCircle);
-
-document.querySelector('button:nth-child(6)').addEventListener('click', selectCross);
-
-document.querySelector('button:nth-child(7)').addEventListener('click', selectCircle);
-
-document.querySelector('button:nth-child(8)').addEventListener('click', selectCross);
-
-document.querySelector('button:nth-child(9)').addEventListener('click', selectCircle);
-
-document.querySelector('button:nth-child(10)').addEventListener('click', selectCross);
+document.querySelector('button:nth-child(1)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(2)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(3)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(4)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(5)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(6)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(7)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(8)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(9)').addEventListener('click', CircleorCross);
+document.querySelector('button:nth-child(10)').addEventListener('click', CircleorCross);
 
 
