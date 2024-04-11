e969f9b03335f023d92c8f47406b3bb6d9bf171b
Olga Pogrebňa <olga.pogrebnya@live.com>
2024-04-11 11:47:25 +0200

    Change structure hra.html, change width button, add in hra.js addEventListener to button

--- hra.js
+++ hra.js
@@ -4,23 +4,29 @@ const selectCircle = (event) => {
   event.target.classList.add('board__field--circle');
 };
 
-const buttonsRow1 = document.querySelectorAll('.button-row1 .button');
-buttonsRow1.forEach((button) => {
-  button.addEventListener('click', selectCircle);
-});
-
-
-/*const button1Row1 = document.querySelector('.button-row1:nth-child(1) .button');
-
-button1Row1.addEventListener('click', selectCircle);
-
-
 const selectCross = (event) => {
   event.target.classList.add('board__field--cross');
 };
 
-const button2Row1 = document.querySelector('.button-row1:nth-child(2) .button');
 
-button2Row1.addEventListener('click', selectCross);*/
+document.querySelector('button:nth-child(1)').addEventListener('click', selectCircle);
+
+document.querySelector('button:nth-child(2)').addEventListener('click', selectCross);
+
+document.querySelector('button:nth-child(3)').addEventListener('click', selectCircle);
+
+document.querySelector('button:nth-child(4)').addEventListener('click', selectCross);
+
+document.querySelector('button:nth-child(5)').addEventListener('click', selectCircle);
+
+document.querySelector('button:nth-child(6)').addEventListener('click', selectCross);
+
+document.querySelector('button:nth-child(7)').addEventListener('click', selectCircle);
+
+document.querySelector('button:nth-child(8)').addEventListener('click', selectCross);
+
+document.querySelector('button:nth-child(9)').addEventListener('click', selectCircle);
+
+document.querySelector('button:nth-child(10)').addEventListener('click', selectCross);
 
 
