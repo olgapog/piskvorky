3d3c03f6e1c050730786df750734a02d28070af8
Olga Pogrebňa <olga.pogrebnya@live.com>
2024-04-28 15:13:02 +0200

    Add AI player through API request

--- hra.js
+++ hra.js
@@ -3,7 +3,10 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 const buttonAll = document.querySelectorAll('button');
 let currentPlayer = 'circle';
 
-const CircleorCross = (event) => {
+
+
+
+const CircleorCross = async (event) => {
 
 if (currentPlayer === 'circle') {
   event.target.classList.add('board__field--circle');
@@ -33,17 +36,44 @@ else {
 });
 
 const winner = findWinner(symbol);
+
 if (winner === 'o' || winner === 'x') {
   setTimeout(() => {
   alert(`Win player with symbol ${winner}!`);
   location.reload();},200 );
 }
+
 else if (winner === 'tie') {
   setTimeout(() => {
   alert('The play is tie!');
    location.reload();}, 200 );
+} 
+
+// Ukol #5
+
+else {
+if (currentPlayer === 'cross'){
+  const response = await fetch("https://piskvorky.czechitas-podklady.cz/api/suggest-next-move",
+{
+  method: "POST",
+  headers: {
+    "Content-type": "application/json",
+  },
+  body: JSON.stringify({
+    board: symbol,
+    player: "x",
+  }),
+});
+
+const data = await response.json();
+const { x, y } = data.position;
+const index = buttonAll[x + y * 10];
+
+index.click();
+}
 }
 };
+
 
 buttonAll.forEach((btn) => {
  btn.addEventListener('click', CircleorCross);
